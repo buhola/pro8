e22698e67a5c605c8fea4da1bf5574eb1fa4e522
zjceo <zrenrique033@gmail.com>
2025-12-18 21:24:40 -0500

    Update organization name in GitHub pages deployment config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -25,7 +25,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "fastura", // Usually your GitHub org/user name.
+  organizationName: "zjceo", // Usually your GitHub org/user name.
   projectName: "pro8", // Usually your repo name.
 
   onBrokenLinks: "throw",
